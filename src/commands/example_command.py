8509045b9ccec9064dea4b74e3b9e5403e483b8b
albertalexandrov <albert-ugatu@list.ru>
2025-08-02 14:59:49 +0300

    команды

---
+++ src/commands/example_command.py
@@ -0,0 +1,2 @@
+async def example_command(message: str):
+    print(f'Вы ввели следующее сообщение: "{message}"')
--- src/manage.py
+++ src/manage.py
@@ -1,12 +1,14 @@
 import os
-import sys
 from fastapi_django.management import cli
+from fastapi_django.management.utils import register_command
+
+from commands.example_command import example_command
 
 
 def main():
-    os.environ.setdefault('FASTAPI_DJANGO_SETTINGS_MODULE', 'settings')
-    cli(sys.argv)
-
+    os.environ.setdefault("FASTAPI_DJANGO_SETTINGS_MODULE", "settings")
+    register_command(example_command)
+    cli()
 
 if __name__ == '__main__':
     main()
